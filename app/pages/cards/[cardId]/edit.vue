b7f3fa3930988cd0fc7ea146846be459a5cbefa9
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2026-05-09 18:11:55 +0530

    Bug fix

--- app/pages/cards/[cardId]/edit.vue
+++ app/pages/cards/[cardId]/edit.vue
@@ -1,7 +1,7 @@
 <template>
   <main v-if="card && deck">
     <nav class="p-18-0 flex align-i-center gap-6 icon-color-white-two">
-      <NuxtLink :to="`/cards/${deck.id}`" class="flex align-i-center">
+      <NuxtLink :to="`/cards/${card.id}`" class="flex align-i-center">
         <backIcon class="w-24" />
       </NuxtLink>
       <h2 class="m-0 color-green-one" font="w-600">Edit Card</h2>
