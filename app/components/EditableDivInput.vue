26765be4188f0e490635beeca1956566779f0f09
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2026-05-24 23:48:57 +0530

    editable div updates

--- app/components/EditableDivInput.vue
+++ app/components/EditableDivInput.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div
-      v-show="answer.length !== 0 && showDoneButton"
+      v-show="showNewLineButton"
       class="absolute r-12 t-12 flex align-i-center gap-4"
     >
       <button
-        class="bg-color-green-two no-bg no-outline border-none pointer p-2-4 icon-color-green-two"
+        class="bg-color-green-two no-outline border-none pointer p-8 grid place-i-center icon-color-blue-one"
         border="rad-10"
         ref="doneButton"
-        @click="handleDone"
+        @click="addNewLine"
       >
-        <doneIcon class="w-32 h-32" />
+        <enterNewLineIcon class="w-22 h-22" />
       </button>
     </div>
     <div
@@ -22,11 +22,13 @@
       <div
         contenteditable="true"
         ref="answerInput"
+        enterkeyhint="go"
         class="ta-center [&:focus]:no-outline color-white-one min-h-1em min-w-1"
         font="s-1.5rem w-425 f-default-font"
         @keyup="answer = ($refs.answerInput as HTMLElement).innerText.trim()"
+        @keydown.enter.prevent="handleEnter"
         @blur="handleBlur"
-        @focus="showDoneButton = true"
+        @focus="showNewLineButton = true"
       ></div>
       <span
         v-show="answer.length === 0"
@@ -44,14 +46,34 @@ export default defineNuxtComponent({
   emits: ["checkAnswer"],
   data: () => ({
     answer: "",
-    showDoneButton: false,
+    showNewLineButton: false,
   }),
   methods: {
-    handleDone() {
-      this.$emit("checkAnswer");
-      this.showDoneButton = false;
+    addNewLine() {
+      const sel = window.getSelection()!;
+      const range = sel.getRangeAt(0);
+
+      // 1. Create a fragment with a BR and a 'propper' (Zero Width Space)
+      // The \u200B makes the line "exist" so the cursor can land on it
+      const br = document.createElement("br");
+      const textNode = document.createTextNode("\u200B");
+
+      range.deleteContents();
+      range.insertNode(textNode);
+      range.insertNode(br);
+
+      // 2. Position the cursor exactly on the TextNode after the BR
+      range.setStart(textNode, 0);
+      range.setEnd(textNode, 0);
+
+      sel.removeAllRanges();
+      sel.addRange(range);
     },
-    handleBlur(e: FocusEvent) {
+    handleEnter(e: Event) {
+      this.$emit("checkAnswer");
+      (e.target as HTMLInputElement).blur();
+    },
+    handleBlur() {
       const answerInput = this.$refs.answerInput as Element;
       const doneButton = this.$refs.doneButton as Element;
       setTimeout(() => {
@@ -60,8 +82,8 @@ export default defineNuxtComponent({
             document.activeElement,
           )
         )
-          this.showDoneButton = false;
-      }, 100);
+          this.showNewLineButton = false;
+      }, 50);
     },
     clearAnswer() {
       this.answer = "";
