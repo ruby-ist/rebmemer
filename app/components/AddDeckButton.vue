7ea5642cccad54a03fe068e3204ff6ad80bca0c5
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2026-05-25 12:18:07 +0530

    add deck button bug fix

--- app/components/AddDeckButton.vue
+++ app/components/AddDeckButton.vue
@@ -1,7 +1,10 @@
 <template>
-  <NuxtLink to="/decks/new">
+  <NuxtLink
+    class="absolute b-48 l-50p translate-x--50p pointer"
+    to="/decks/new"
+  >
     <button
-      class="h-88 w-88 absolute b-48 l-50p translate-x--50p pointer grid place-i-center bg-color-green-two icon-color-indigo-one active:bg-color-green-one active:scale-0.96"
+      class="h-88 w-88 grid place-i-center bg-color-green-two icon-color-indigo-one active:bg-color-green-one active:scale-96"
       border="none rad-50"
     >
       <PlusIcon class="w-48" />
--- pwa.config.ts
+++ pwa.config.ts
@@ -10,7 +10,7 @@ const config: Partial<ModuleOptions> = {
     short_name: "Rebememer",
     start_url: "/decks",
     description: "Mobile first offline Spacial Repitation System as PWA",
-    theme_color: "#232b5f",
+    theme_color: "#1D528A",
     icons: [
       {
         src: "pwa-64x64.png",
